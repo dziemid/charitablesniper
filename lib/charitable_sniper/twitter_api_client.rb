4b9232d5d19ae0819cb9556c56a6166dee5848da
Greg Dziemidowicz <greg@cogent.co>
2013-10-20 18:06:47 +1100

    Tweeting

--- lib/charitable_sniper/twitter_api_client.rb
+++ lib/charitable_sniper/twitter_api_client.rb
@@ -3,7 +3,9 @@ module CharitableSniper
   class TwitterApiClient
 
     def action_for profile
-      puts "action: #{profile} "
+      message = "Donate $#{profile.target_to_go} to fund treatment for #{profile.name} ( #{profile.url} )"
+      puts message
+      Twitter.update(message)
     end
 
   end
